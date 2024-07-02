b55428cf50ddd6775390d55d3e3d6f390b29ee58
dvidyara <dvidyara@cisco.com>
2024-07-02 12:21:42 -0700

    Data Validation Stages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "DATA INGESTION STAGE"
 try:
@@ -10,3 +11,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "DATA VALIDATION STAGE"
+try:
+    logger.info(f">>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<<<<<<")
+    obj= DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage { STAGE_NAME} Completed <<<<<<<<<<<<<<<<<<\n\nx===================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
